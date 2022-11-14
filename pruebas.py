d24c5a74b030ead9866dc7f7891dba1b2864ad94
AND3SIL4 <104874345+AND3SIL4@users.noreply.github.com>
2022-11-14 15:12:53 -0500

    Inicio py

--- pruebas.py
+++ pruebas.py
@@ -1,1 +1,7 @@
-print("HOLA MI PERRO")
+print("HOLA MI PERRO\n")
+
+nombre=input("Ingrese su nombre mi perro: ")
+edad=int(input("Ingrese su edad mi perro: "))
+
+print (f"Usted se llama", nombre, "y tiene", edad, "años")
+
